7bb9a329a96f52c96e8be802d0f2cbf5302fcd42
arslansD <55465158+arslansD@users.noreply.github.com>
2024-03-08 00:16:54 +0600

    List and Details api views added

--- django_project/settings.py
+++ django_project/settings.py
@@ -42,8 +42,14 @@ INSTALLED_APPS = [
     'todos',
 
     #thirdparty
-
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,3 +133,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title='Test',
+            body='Test body'
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, 'Test')
+        self.assertEqual(self.todo.body, 'Test body')
+        self.assertEqual(str(self.todo), 'Test')
